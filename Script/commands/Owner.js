2f8e27e6a9fa448f221a0edf9b15660300fd0380
𝐋𝐗 𝐀𝐑𝐀𝐅𝐀𝐓 <skrahad774@gmail.com>
2026-09-10 12:30:06 +0600

    Add owner command with styled info and random image

--- Script/commands/Owner.js
+++ Script/commands/Owner.js
@@ -1,1 +1,61 @@
+const request = require("request");
+const fs = require("fs-extra");
 
+module.exports.config = {
+  name: "owner",
+  version: "1.0.1",
+  hasPermssion: 0,
+  credits: "LX ARAFAT",
+  description: "Show Owner Info with styled box & random photo",
+  commandCategory: "Information",
+  usages: "owner",
+  cooldowns: 2
+};
+
+module.exports.run = async function ({ api, event }) {
+
+  
+  const info = `
+╔═════════════════════ ✿
+║ ✨ 𝗢𝗪𝗡𝗘𝗥 𝗜𝗡𝗙𝗢 ✨
+╠═════════════════════ ✿
+║ 👑 𝗡𝗮𝗺𝗲 : 𝗟𝗫 𝗔𝗥𝗔𝗙𝗔𝗧
+║ 🧸 𝗡𝗶𝗰𝗸 𝗡𝗮𝗺𝗲 : 𝗔𝗥𝗔𝗙𝗔𝗧
+║ 🎂 𝗔𝗴𝗲 : 𝟭𝟳+
+║ 💘 𝗥𝗲𝗹𝗮𝘁𝗶𝗼𝗻 : 𝗦𝗶𝗻𝗴𝗹𝗲
+║ 🎓 𝗣𝗿𝗼𝗳𝗲𝘀𝘀𝗶𝗼𝗻 : 𝗦𝘁𝘂𝗱𝗲𝗻𝘁
+║ 📚 𝗘𝗱𝘂𝗰𝗮𝘁𝗶𝗼𝗻 : 𝗦𝗦𝗖 (𝟮𝟬𝟮𝟳)
+║ 🏡 𝗔𝗱𝗱𝗿𝗲𝘀𝘀 : 𝗕𝗼𝗴𝘂𝗿𝗮
+╠═════════════════════ ✿
+║ 🔗 𝗖𝗢𝗡𝗧𝗔𝗖𝗧 𝗟𝗜𝗡𝗞𝗦
+╠═════════════════════ ✿
+║ 📘 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 :
+║ fb.com/61581249425737
+║ 💬 𝗠𝗲𝘀𝘀𝗲𝗻𝗴𝗲𝗿 :
+║ m.me/61581249425737
+║ 📞 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 :
+║ wa.me/+8801826618633
+║ ✈️ 𝗧𝗲𝗹𝗲𝗴𝗿𝗮𝗺 :
+║ t.me/lxarafat639
+╚═════════════════════ ✿
+`;
+
+  const images = [
+    "https://i.imgur.com/PWviWoq.jpeg"
+  ];
+
+  const randomImg = images[Math.floor(Math.random() * images.length)];
+
+  const callback = () => api.sendMessage(
+    {
+      body: info,
+      attachment: fs.createReadStream(__dirname + "/cache/owner.jpg")
+    },
+    event.threadID,
+    () => fs.unlinkSync(__dirname + "/cache/owner.jpg")
+  );
+
+  return request(encodeURI(randomImg))
+    .pipe(fs.createWriteStream(__dirname + "/cache/owner.jpg"))
+    .on("close", () => callback());
+};
