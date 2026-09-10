4e21d330ee45b999cf13a878caf72f48e6efc4d2
𝐋𝐗 𝐀𝐑𝐀𝐅𝐀𝐓 <skrahad774@gmail.com>
2026-09-10 22:13:30 +0600

    Update Autoreply.js

--- Script/commands/Autoreply.js
+++ Script/commands/Autoreply.js
@@ -8,7 +8,7 @@ module.exports.config = {
   name: "autoreplybot",
   version: "2.0.0",
   hasPermssion: 0,
-  credits: "LX ARAFAT",
+  credits: "SHAHADAT SAHU",
   usePrefix: false,
   commandCategory: "Chat",
   cooldowns: 0
